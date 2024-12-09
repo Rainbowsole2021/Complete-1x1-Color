18d86c274594d93855683e6ec8bca561e29af3f9
Rainbowsole2021 <122910261+Rainbowsole2021@users.noreply.github.com>
2024-12-09 13:33:20 +0100

    Christmas Edition
    With music

--- Complete_1x1_Color/Complete_1x1_Color/Program.cs
+++ Complete_1x1_Color/Complete_1x1_Color/Program.cs
@@ -1,19 +1,33 @@
-﻿namespace Complete_1x1_Color
+﻿using NAudio.Wave;
+
+namespace Complete_1x1_Color
 {
     internal class Program
     {
+        private static IWavePlayer waveOutDevice;
+        private static AudioFileReader audioFileReader;
+
         static void Main(string[] args)
         {
-            Random random = new Random();
-            ConsoleColor[] color = Enum.GetValues<ConsoleColor>();
+            ConsoleColor[] colors =
+            {
+                ConsoleColor.Red, ConsoleColor.White, ConsoleColor.Green
+            };
+            string musicPath = Directory.GetCurrentDirectory() + @"/Music.mp3";
+            int colorCode = -1;
 
+            Play(musicPath);
             for (int i = 1; i <= 10; i++)
             {
                 Console.WriteLine($"Reihe {i}");
 
                 for (int j = 1; j <= 10; j++)
                 {
-                    Console.ForegroundColor = color[random.Next(0, color.Length)];
+                    colorCode++;
+                    if (colorCode >= colors.Length)
+                        colorCode = 0;
+
+                    Console.ForegroundColor = colors[colorCode];
                     Console.WriteLine($"{i}".PadRight(2) + " * " + $"{j}".PadRight(2) + " = " + $"{i * j}".PadRight(3));
                 }
 
@@ -21,6 +35,15 @@
             }
 
             Console.ReadKey();
+        }
+
+        private static void Play(string filePath)
+        {
+            waveOutDevice = new WaveOutEvent();
+            audioFileReader = new AudioFileReader(filePath);
+
+            waveOutDevice.Init(audioFileReader);
+            waveOutDevice.Play();
         }
     }
 }
